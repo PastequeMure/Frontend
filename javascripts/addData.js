bc4d6d5e284e8ffe758b0432a5a1b42ded8379be
PastequeMure <139119250+PastequeMure@users.noreply.github.com>
2023-10-01 02:01:26 +0200

    add css to frontend main

--- javascripts/addData.js
+++ javascripts/addData.js
@@ -7,8 +7,20 @@ export function addData() {
         for(let i = 0; i < data.length; i++) {
             const ChallengeA = document.createElement('a');
             ChallengeA.href = `./${data[i]}`;
-            ChallengeA.innerText = `#${i+1} ${data[i]}`;
             cardDiv.appendChild(ChallengeA)
+
+            const challengeDiv = document.createElement('div');
+            ChallengeA.appendChild(challengeDiv);
+
+            const imgA = document.createElement('img');
+            imgA.src = `./screenshots/${data[i]}.png`;
+            imgA.alt = data[i];
+            challengeDiv.appendChild(imgA);
+
+            const pChallenge = document.createElement('p');
+            pChallenge.innerText = `#${i+1} ${data[i]}`;
+            challengeDiv.appendChild(pChallenge)
+
         }
     })
 }
